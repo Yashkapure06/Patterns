1b16a58645fd0bdf13b56017f57318c3ede10e1a
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-08-07 23:51:41 +0530

    Right Triangle Star

---
+++ src/Patterns/RightTriangle.java
@@ -0,0 +1,21 @@
+package Patterns;
+
+public class RightTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//i for rows and j for columns
+		//row denotes the number of rows you want to print
+		int i, j, row = 6;
+		//outer for loop -> for rows
+		for (i = 0; i < row; i++) {
+			//inner for loop -> for columns
+			for (j = 0; j <=i; j++) {
+					//print stars
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
