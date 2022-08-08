9e0852bf58a17dfd565fea9e3b275511fc4ba3c8
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-08-08 21:35:48 +0530

    Left Triangle Star

---
+++ src/Patterns/LeftTriangle.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+public class LeftTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// i = row, j = col
+		int i, j, row = 6;
+
+		for (i = 0; i < row; i++) {
+
+			for (j = 2 * (row - i); j >= 0; j--) {
+
+				System.out.print(" ");
+			}
+			for (j = 0; j <= i; j++) {
+				System.out.print("* ");
+			}
+			
+			// throws the cursor in a new line after printing each line
+			System.out.println();
+		}
+	}
+
+}
