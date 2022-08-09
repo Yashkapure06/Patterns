d95dab85d9333b6303f446736e6bf03dddcb9d93
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-08-09 23:11:17 +0530

    Pyramid

---
+++ src/PyramidPatterns/Pyramid.java
@@ -0,0 +1,29 @@
+package PyramidPatterns;
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, j, row = 6 ;
+		
+		
+		for (i = 0; i < row; i++) {
+			
+			
+			for (j = row-i; j >1; j--) {
+				System.out.print(" ");
+			}
+			
+			
+			for (j = 0; j <= i; j++) {
+				System.out.print("* ");
+			}
+			// throws the cursor in a new line after printing each line
+			System.out.println();
+			
+		}
+		
+		
+	}
+
+}
