9327f4619ef37c6bd4d36e774a12a49d147efda5
shafi <mdshafiuddin935@gmail.com>
2023-10-01 11:48:27 +0530

    added Triangle Star Pattern.

---
+++ src/TriangleStarPattern/TriangleStarPattern.java
@@ -0,0 +1,32 @@
+package TriangleStarPattern;
+
+public class TriangleStarPattern {
+
+    public static void main(String[] args) {
+        // Declare variables i, j, k, and set the number of rows to 9
+        int i, j, k, rows = 9;
+
+        // Loop through rows from 1 to 9
+        for (i = 1; i <= rows; i++) {
+            // Print spaces before the pattern to center it
+            for (j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+
+            // Loop to print the pattern for the current row
+            for (k = 1; k <= (2 * i - 1); k++) {
+                // If it's the first character, last character, or the bottom row, print '*'
+                if (k == 1 || i == rows || k == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    // Otherwise, print a space to create the hollow pattern
+                    System.out.print(" ");
+                }
+            }
+
+            // Move to the next line for the next row
+            System.out.println("");
+        }
+
+    }
+}
