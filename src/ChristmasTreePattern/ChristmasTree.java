b53c7141f21adf7db76c56ff18e40f70081751d3
John Carl Quieta <77482435+jcgaming-official@users.noreply.github.com>
2023-10-07 16:34:08 +0800

    Update ChristmasTree.java

--- src/ChristmasTreePattern/ChristmasTree.java
+++ src/ChristmasTreePattern/ChristmasTree.java
@@ -2,21 +2,21 @@ package ChristmasTreePattern;
 
 public class ChristmasTree {
     public static void main(String[] args) {
-        int height = 7; // Adjust the height of the banana pattern as needed
+        int height = 7;
 
         for (int i = 1; i <= height; i++) {
             for (int j = 1; j <= height - i; j++) {
-                System.out.print(" "); // Print spaces for the left side of the banana
+                System.out.print(" "); 
             }
 
             for (int j = 1; j <= 2 * i - 1; j++) {
-                System.out.print("*"); // Print asterisks for the curved part of the banana
+                System.out.print("*"); 
             }
 
-            System.out.println(); // Move to the next line
+            System.out.println(); 
         }
 
-        // Print the stem of the banana
+        
         for (int i = 1; i <= height / 2; i++) {
             for (int j = 1; j <= height - 1; j++) {
                 System.out.print(" ");
